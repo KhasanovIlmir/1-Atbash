d75716cd82cffab3e3e625da661dedf506b797b9
Ильмир <hasanovilmir9@gmail.com>
2024-10-30 13:05:58 +0400

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+char atbash(char text) {
+    if (text >= 'a' && text <= 'z') {
+        return 'z' - (text - 'a');
+    }
+    else if (text >= 'A' && text <= 'Z') {
+        return 'Z' - (text - 'A');
+    }
+    else if (text >= 'а' && text <= 'я') {
+        return 'я' - (text - 'а');
+    }
+    else if (text >= 'А' && text <= 'Я') {
+        return 'Я' - (text - 'А');
+    }
+    return text;
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    std::string result;
+    std::string inputText;
+
+    std::cout << "Введите текст для шифра: ";
+    std::getline(std::cin, inputText);
+
+    for (char charInput : inputText) {
+        result += atbash(charInput);
+    }
+
+    std::cout << result << std::endl;
+    return 0;
+}
